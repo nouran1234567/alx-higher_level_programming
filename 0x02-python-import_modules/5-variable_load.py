ca756069142eace2705bff5efe809be513eecf22
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-07 19:46:44 -0700

    5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,5 +3,5 @@
 # program that imports a variable from a file and prints its value
 
 if __name__ == "__main__":
-    from variable_load_5
-    print(variable_load_5.a)
+    from variable_load_5 import a
+    print(a)
