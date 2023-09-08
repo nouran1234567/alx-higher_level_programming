d006610d8819d777375af9f882792a0701c59ee3
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-07 19:24:06 -0700

    2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# Import a simple function from a simple file
+
 if __name__ == "__main__":
     from add_0 import add
     a = 1
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# program that prints the number of and the list of its arguments
+
 if __name__ == "__main__":
     import sys
     x = len(sys.argv) - 1
