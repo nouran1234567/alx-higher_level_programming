45773e1f0a55e04eca103a0a6a274f3c08144a2b
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-11 19:21:01 -0700

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,27 +2,27 @@
 
 # function that adds 2 tuples
 
-def add_tuple(tuple_z=(), tuple_y=()):
-    len_z = len(tuple_z)
-    len_y = len(tuple_y)
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_z = len(tuple_a)
+    len_y = len(tuple_b)
     if len_z == 0:
         z1 = 0
         z2 = 0
     elif len_z == 1:
-        z1 = tuple_z[0]
+        z1 = tuple_a[0]
         z2 = 0
     else:
-        z1 = tuple_z[0]
-        z2 = tuple_z[1]
+        z1 = tuple_a[0]
+        z2 = tuple_a[1]
     if len_y == 0:
         y1 = 0
         y2 = 0
     elif len_y == 1:
-        y1 = tuple_y[0]
+        y1 = tuple_b[0]
         y2 = 0
     else:
-        y1 = tuple_y[0]
-        y2 = tuple_y[1]
-    own_tup = (z1 + z1, y2 + y2)
+        y1 = tuple_b[0]
+        y2 = tuple_b[1]
+    own_tup = (z1 + y1, z2 + y2)
 
     return (own_tup)
