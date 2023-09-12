e9313ad35a79180f9f8d065a99f607a9360f60ae
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-11 18:46:29 -0700

    1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# function that retrieves an element from a list like in C
+
+def element_at(my_list, idx):
+    if (idx > len(my_list) - 1) or (idx < 0):
+        return None
+    return(my_list[idx])
