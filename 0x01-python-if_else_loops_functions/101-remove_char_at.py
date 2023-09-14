46c01c015a205fc47d60459fa3c1f5bc627d69f1
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 00:04:14 -0700

    101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+# function that creates a copy of the string
+
+def remove_char_at(str, n):
+    x = ""
+    z = 0
+    for c in str:
+        if z != n:
+            x += c
+        z += 1
+    return x
