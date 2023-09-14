75ddb81f03843c94aab60aa406dadfebaf510dee
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 03:05:29 -0700

    2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# function that adds all unique integers in a list
+
+def uniq_add(my_list=[]):
+    own_list = set(my_list)
+    z = 0
+    for y in own_list:
+        num += y
+
+    return (z)
