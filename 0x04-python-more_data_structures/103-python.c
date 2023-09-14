81a20bc382bdfdbab569db52fb9c09c8b389a505
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 04:41:46 -0700

    103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -71,7 +71,7 @@ void print_python_list(PyObject *p)
 	for (x = 0; x < sz; x++)
 	{
 		obj = ((PyListObject *)p)->ob_item[x];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		printf("Element %ld: %s\n", x, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
