deff0ecf1dfdd9b8da819797e6ccb6d58a319ad1
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 03:00:36 -0700

    0

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# function that replaces all occurrences of an element by another
+
+def search_replace(my_list, search, replace):
+    return [replace if search == z else z for z in my_list]
