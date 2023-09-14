026862daf06534865c25e6635871cdbd261a924a
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-13 23:55:36 -0700

    102

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+# Python function def magic_calculation(a, b, c)
+
+def magic_calculation(a, b, c):
+    if c > b:
+        return a + b
+    if a < b:
+        return c
+    return (a * b) - c
+
