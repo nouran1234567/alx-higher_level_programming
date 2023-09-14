e820ca6aae36e1631ea036fb43ca9d0271578178
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 03:36:03 -0700

    12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# Create a function def roman_to_int(roman_string)
+
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    info = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[y] for y in roman_string] + [0]
+    agi = 0
+    for z in range(len(numbers) - 1):
+        if numbers[z] >= numbers[z+1]:
+            agi += numbers[z]
+        else:
+            agi -= numbers[z]
+
+    return agi
+
