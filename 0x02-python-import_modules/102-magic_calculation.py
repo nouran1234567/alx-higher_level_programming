e760b73ccfa76e60734ce1d7238186de17ee964e
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 01:03:55 -0700

    102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,9 @@
 # Python function def magic_calculation(a, b)
 
 from magic_calculation_102 import add, sub
+
 def magic_calculation(a, b):
+
     if a < b:
         c = add(a, b)
         for x in range(4, 6):
