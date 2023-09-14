1c8af73d1848b6c645bdad30159b4f32a9c34b4b
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-14 03:32:23 -0700

    11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-# function returns list with all values multiplied by number
-
 def multiply_list_map(my_list=[], number=0):
     return (list(map((lambda z: z * number), my_list)))
