ad1292715a6ae970695762f101dcdda78590aff5
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-25 16:01:10 -0700

    101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# function that executes a function safely
+
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return (rzlt)
+    except Exception as z:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+
+        return (None)
